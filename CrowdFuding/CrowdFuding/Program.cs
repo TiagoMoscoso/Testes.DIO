2d0b2e3f01f8b91b892854ae8808c3b4c14d5840
TiagoMoscoso <tiagocarvalhomoscoso@gmail.com>
2021-04-19 21:26:10 -0300

    primeiro commit

---
+++ CrowdFuding/CrowdFuding/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CrowdFuding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
